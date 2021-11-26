504317e7235c64941ab4d40cba6b2dad405b8d5f
Giulia Sardell <giuliasardelli1234@gmail.com>
2021-11-26 12:22:58 +0100

    correzione img

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="product-card">
-    <img :src="productImage" :alt="productName">
+    <div class="container-card">
+      <img :src="productImage" :alt="productName">
+      
+    </div>
     <h3>{{productName}}</h3>
 
     
   </div>
+  
 </template>
 
 <script>
@@ -25,19 +29,35 @@ export default {
 
 .product-card{
   // flex-basis: 12%;
-  width: 150px;
-  height: 150px;
-  margin: 40px 10px;
-  img{
-    height: 100%;
+  width: calc(100% / 6 - 40px);
+  // padding-top: calc(100% / 6 - 20px);
+  // height: 150px;
+  // margin: 40px 10px;
+  margin: 25px 20px;
+  cursor: pointer;
+ 
+  .container-card{
     width: 100%;
+    height: 0;
+    padding-top: 100%;
+    position: relative;
     overflow: hidden;
+    
+
+  }
+  img{
+    // height: 100%;
+    width: 100%;
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
   }
 
   h3{
     color: white;
     font-size: 14px;
-    margin-bottom: 30px;
+    margin: 20px 0;
   }
 
   
